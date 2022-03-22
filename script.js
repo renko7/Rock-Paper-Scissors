839a11ce7ecf4765d9be22e5492e891860beffd3
renko7 <66572823+renko7@users.noreply.github.com>
2022-03-22 18:49:11 -0400

    base play game code

---
+++ script.js
@@ -0,0 +1,53 @@
+function computerPlay() { 
+    const options = ['rock', 'paper', 'scissors']
+    const randomNumber = Math.floor(Math.random() * 3)
+    return options.at(randomNumber)
+}
+
+function playerPlay() {
+    const playerChoice = prompt("Enter Rock, Paper, or Scissors: ").toLowerCase()
+    return playerChoice
+}
+
+function detectWinnerCondition(playerChoice) {
+    winnerCondition = ''
+    switch(playerChoice) {
+        case "rock":
+            winnerCondition = "paper"
+            break
+        case "paper":
+            winnerCondition = "scissors"
+            break
+        case "scissors":
+            winnerCondition = "rock"
+            break
+    }
+    return winnerCondition
+}
+
+function playRound(computerChoice, playerChoice) {
+    winnerCondition = detectWinnerCondition(computerChoice)
+
+    if(playerChoice === computerChoice) {
+        console.log('Tie game!')
+    } else if(playerChoice === winnerCondition) {
+        console.log('You win! %s beats %s', playerChoice, computerChoice)
+    } else {
+        console.log('You lose! %s beats %s', computerChoice, playerChoice)
+    }
+}
+
+function playGame(rounds) {
+    roundsPlayed = 0
+
+
+    while (roundsPlayed < 5) {
+        const computerChoice = computerPlay()
+        const playerChoice = playerPlay()
+        playRound(computerChoice, playerChoice)
+        roundsPlayed += 1
+    }
+}
+
+
+playGame(5)
