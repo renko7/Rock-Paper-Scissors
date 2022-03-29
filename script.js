1020fdbf16678cf72865a0332c6d74fc76a609ae
renko7 <66572823+renko7@users.noreply.github.com>
2022-03-29 01:41:06 -0400

    added growth to options on mouseover, added message of outcome for playround, added click handler for options

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+let playerChoice = ''
+
+const computerChoiceElement = document.querySelector('section.computer .choice')
+
+const playerOptions = document.querySelectorAll('section.content img')
+
+playerOptions.forEach( option => option.addEventListener('click', (e) => {
+    playerChoice = e.target.alt
+    playGame()
+}))
+
+
 function computerPlay() { 
     const options = ['rock', 'paper', 'scissors']
     const randomNumber = Math.floor(Math.random() * 3)
     return options.at(randomNumber)
-}
-
-function playerPlay() {
-    const playerChoice = prompt("Enter Rock, Paper, or Scissors: ").toLowerCase()
-    return playerChoice
 }
 
 function detectWinnerCondition(playerChoice) {
@@ -27,36 +34,22 @@ function detectWinnerCondition(playerChoice) {
 
 function playRound(computerChoice, playerChoice) {
     winnerCondition = detectWinnerCondition(computerChoice)
-
+    outcome = ''
     if(playerChoice === computerChoice) {
-        console.log('Tie game!')
+        outcome = 'Tie game!'
     } else if(playerChoice === winnerCondition) {
-        console.log('You win! %s beats %s', playerChoice, computerChoice)
+        outcome = `You lose! ${playerChoice} beats ${computerChoice}.`
     } else {
-        console.log('You lose! %s beats %s', computerChoice, playerChoice)
+        outcome = `You lose! ${computerChoice} beats ${playerChoice}.`
     }
+    return outcome
 }
 
 function playGame(rounds) {
-    roundsPlayed = 0
-
-
-    while (roundsPlayed < 5) {
-        const computerChoice = computerPlay()
-        const playerChoice = playerPlay()
-        playRound(computerChoice, playerChoice)
-        roundsPlayed += 1
-    }
+    const computerChoice = computerPlay()
+    outcome = playRound(computerChoice, playerChoice)
+    computerChoiceElement.textContent = outcome
+    playerChoice = ''
 }
 
 
-playGame(5)
-
-const hand = document.querySelector("div.paper")
-print(hand)
-
-function changeColor(e) {
-    this.style.background = 'green'
-}
-
-hand.addEventListener('mouseOver', () => hand.classList.add('green-background'))
