1df1c00765e5533b51dd6b15e38ea491df40bced
renko7 <66572823+renko7@users.noreply.github.com>
2022-03-29 02:31:16 -0400

    adding scores for player and computer and fixing game result section

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let playerChoice = ''
 
-const computerChoiceElement = document.querySelector('section.computer .choice')
+const gameResult = document.querySelector('section.result p')
 
 const playerOptions = document.querySelectorAll('section.content img')
+
+const playerScore = document.querySelector('section.header .score .player')
+const computerScore = document.querySelector('section.header .score .computer')
 
 playerOptions.forEach( option => option.addEventListener('click', (e) => {
     playerChoice = e.target.alt
@@ -38,7 +41,7 @@ function playRound(computerChoice, playerChoice) {
     if(playerChoice === computerChoice) {
         outcome = 'Tie game!'
     } else if(playerChoice === winnerCondition) {
-        outcome = `You lose! ${playerChoice} beats ${computerChoice}.`
+        outcome = `You win! ${playerChoice} beats ${computerChoice}.`
     } else {
         outcome = `You lose! ${computerChoice} beats ${playerChoice}.`
     }
@@ -48,7 +51,12 @@ function playRound(computerChoice, playerChoice) {
 function playGame(rounds) {
     const computerChoice = computerPlay()
     outcome = playRound(computerChoice, playerChoice)
-    computerChoiceElement.textContent = outcome
+    if(outcome.includes('win')) {
+        playerScore.textContent = String((+playerScore.textContent)+1)
+    } else if(outcome.includes('lose')) {
+        computerScore.textContent = String((+computerScore.textContent)+1)
+    }
+    gameResult.textContent = outcome
     playerChoice = ''
 }
 
